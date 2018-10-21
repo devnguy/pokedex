d19edc1cc5d193d39ad50b05d198322721f0d46b
Dev Nguyen <devnguyen@Rigby-20.local>
2018-10-21 01:23:25 -0500

    Updates

---
+++ precache-manifest.42b80d15be1278ec16509ee76d4e0deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69bab70dd00f4429b781",
+    "url": "/pokedex/static/js/runtime~main.69bab70d.js"
+  },
+  {
+    "revision": "ec36543c48f481d55f64",
+    "url": "/pokedex/static/js/main.ec36543c.chunk.js"
+  },
+  {
+    "revision": "ccf717e8f1c052517a1a",
+    "url": "/pokedex/static/js/1.ccf717e8.chunk.js"
+  },
+  {
+    "revision": "ec36543c48f481d55f64",
+    "url": "/pokedex/static/css/main.b8ab8c65.chunk.css"
+  },
+  {
+    "revision": "74eff82f47c9ff1d585a50e6c46bbdd5",
+    "url": "/pokedex/index.html"
+  }
+];
